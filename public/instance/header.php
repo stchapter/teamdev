b58de1de0f1df124e74161272b3357a662096c15
sakura miyamoto <sakura.m.208@gmail.com>
2021-06-22 00:40:21 +0900

    header.phpを作成

---
+++ public/instance/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/fomantic-ui/2.8.7/semantic.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fomantic-ui/2.8.7/semantic.min.css" media="all">
+  <link rel="icon" href="../../img/favicon.ico">
+  <link rel="stylesheet" href="../../src/css/main.css">
+  <title>GEEKBOOK</title>
+</head>
+<body>
+  <header>
+  <div class="header_container">
+    <div class="header_logo">
+      <img src="../../img/GeekBook-logo.png">
+    </div>
+    <div class="header_button">
+    <div><button class="big ui primary button">TOPへ</button></div>
+    <div><button class="big ui primary button">登録修正</button></div>
+    <div><button class="big ui primary button">新規投稿</button></div>
+    <div><button class="big ui primary button">自分の投稿</button></div>
+    <div><button class="big ui primary button">Bookmark</button></div>
+    <div><button class="big ui primary button">Logout</button></div>
+    </div>
+  </div>
+  </header>
+</body>
+</html>
