e763dad03dc019ca742deb32492e530a501e39a2
atuki <stchapter31@gmail.com>
2021-06-23 03:35:12 +0900

    newpageとDB接続ファイルの作成

---
+++ public/newpage.php
@@ -0,0 +1,90 @@
+<?php
+//session check//
+// session_start();
+// sschk();
+include("../src/php/funcs.php");
+include("../src/php/db_conn.php");
+
+
+/*-------------------------------------------------------------------------
+DB接続（langのプルダウン作成）
+-------------------------------------------------------------------------*/
+//1.  DB接続します
+$pdo = db_conn();
+// $uid = $_SESSION["uid"];
+
+//２．データ登録SQL作成
+$stmt = $pdo->prepare("SELECT * FROM lang_table");
+$status = $stmt->execute();
+
+//３．データ表示
+$view="";  //HTML文字作成を入れる変数
+if($status==false) {
+    //execute（SQL実行時にエラーがある場合）
+    $error = $stmt->errorInfo();
+    exit("SQLError:".$error[2]);
+}else{
+  //Selectデータの数だけ自動でループしてくれる
+  //FETCH_ASSOC=http://php.net/manual/ja/pdostatement.fetch.php
+}
+while( $res = $stmt->fetch(PDO::FETCH_ASSOC)){ 
+    $view .='<option value = "'.$res["lang"].'">'.$res["lang"].'</option>';
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <head><link rel="icon" type="image/x-icon" href="img/favicon.svg"></head>
+        <title>newpage</title>
+    </head>
+    <body>
+        <p>新規投稿</p>
+        <form method="POST" action="../src/php/newpage_insert.php" enctype="multipart/form-data">
+            <p>言語:
+                <select name="lang">
+                    <option value = "選択してください" placeholder="選択してください"></option>
+                    <?=$view?>
+                </select>
+            </p>
+            <p>タイトル(機能):
+                <input type="text" name="title" placeholder="64文字以内で入力してください">
+            </p>
+            <p>おすすめ内容:
+                <textarea name="cont" rows="4" clos="50"></textarea>
+            </p>
+            <p>URL
+                <input type="url" name="url">
+            </p>
+            <p>有料・無料
+                <select name="cost">
+                    <option value ="有料">有料</option>
+                    <option value ="無料">無料</option>
+                </select>
+            </p>
+            <p>投稿
+                <select name="post">
+                    <option value ="投稿">投稿</option>
+                    <option value ="下書き保存">下書き保存</option>
+                </select>
+            </p>
+            <p>おすすめ度
+                <select name="star">
+                    <option value ="5">★★★★★</option>
+                    <option value ="4">★★★★☆</option>
+                    <option value ="3">★★★☆☆</option>
+                    <option value ="2">★★☆☆☆</option>
+                    <option value ="1">★☆☆☆☆</option>
+                </select>
+            </p>
+            <p>添付ファイル
+                <input type="file" accept="image/*, .pdf, .jpg, .jpeg, .png" name="upfile">
+            </p>
+            <input class="cost_submit" type="submit" value="投稿">    
+        </form>
+    </body>
+</html>
---
+++ src/php/newpage_insert.php
@@ -0,0 +1,89 @@
+<?php
+//session check//
+//session_start();
+//sschk();
+include("funcs.php");
+include("db_conn.php");
+
+
+//1. POSTデータ取得
+$lang     = $_POST["lang"];
+$title    = $_POST["title"];
+$cont     = $_POST["cont"];
+$url      = $_POST["url"];
+$cost     = $_POST["cost"];
+$post     = $_POST["post"];
+$star     = $_POST["star"];
+$fpass    = fileUpload("upfile","../../img/");
+$fname    = $_FILES["upfile"]["name"];
+$err_msgs = array();
+
+
+if($lang == "選択してください"){
+  array_push($err_msgs,'言語を選択してください<br><br>');
+}
+if(strlen($title) == 0){
+  array_push($err_msgs,'タイトルを入力してください<br><br>');
+}
+if(strlen($cont) == 0){
+  array_push($err_msgs,'おすすめ内容を入力してください<br><br>');
+}
+if(count($err_msgs) === 0) {
+
+
+/* ------------------------------------------------
+    DBにデータを保存 start
+------------------------------------------------ */
+
+    // 2. DB接続します
+    $pdo = db_conn();
+    // $uid = $_SESSION["uid"];
+    $uid = 1;
+
+
+    //３．データ登録SQL作成
+    $stmt = $pdo->prepare("INSERT INTO post_table(uid,lang,title,cont,url,cost,post,fpass,fname,star,pdate)VALUES($uid,:lang,:title,:cont,:url,:cost,:post,:fpass,:fname,:star,sysdate())");
+    $stmt->bindValue(':lang', $lang, PDO::PARAM_STR);      //Integer（数値の場合 PDO::PARAM_INT)
+    $stmt->bindValue(':title', $title, PDO::PARAM_STR);      //Integer（数値の場合 PDO::PARAM_INT)
+    $stmt->bindValue(':cont', $cont, PDO::PARAM_STR);      //Integer（数値の場合 PDO::PARAM_INT)
+    $stmt->bindValue(':url', $url, PDO::PARAM_STR);      //Integer（数値の場合 PDO::PARAM_INT)
+    $stmt->bindValue(':cost', $cost, PDO::PARAM_STR);      //Integer（数値の場合 PDO::PARAM_INT)
+    $stmt->bindValue(':post', $post, PDO::PARAM_STR);      //Integer（数値の場合 PDO::PARAM_INT)
+    $stmt->bindValue(':fpass', $fpass, PDO::PARAM_STR);      //Integer（数値の場合 PDO::PARAM_INT)
+    $stmt->bindValue(':fname', $fname, PDO::PARAM_STR);      //Integer（数値の場合 PDO::PARAM_INT)
+    $stmt->bindValue(':star', $star, PDO::PARAM_INT);      //Integer（数値の場合 PDO::PARAM_INT)
+    $status = $stmt->execute(); //実行
+
+    echo '登録が完了しました<br><br><br><br>';
+    echo '<a href="../public/newpage.php">登録画面に戻る</a>';
+    /* ------------------------------------------------
+    //////DBにデータを保存 end
+    ------------------------------------------------ */
+
+} else{
+  foreach ($err_msgs as $msg){
+    echo $msg;
+    echo '<br>';
+  }
+  echo '<br>';
+  echo '<a href="../public/newpage.php">登録画面に戻る</a><br><br>';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <head><link rel="icon" type="image/x-icon" href="img/favicon.svg"></head>
+        <link href="css/reset.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.3.2/chart.min.js"></script>
+        <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.1.0/css/line-awesome.min.css">
+        <title>エラー画面</title>
+    </head>
+<body class="err_body">
+
+</body>
+</html>
