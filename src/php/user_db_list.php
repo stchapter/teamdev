a2e965c86f09acd0fff52e764baed8e236e746fb
jun <junf2012@gmail.com>
2021-06-23 22:41:09 +0900

    entry_php会員登録関連

---
+++ src/php/user_db_list.php
@@ -0,0 +1,71 @@
+
+<?php
+
+//campデータのリストだし
+function camp_l(){
+  $stmt = $pdo->prepare("SELECT * FROM camp_table");
+  $status = $stmt->execute();
+
+  if($status==false) {
+    $error = $stmt->errorInfo();
+    exit("SQLエラー:".$error[2]);
+  }else{
+    $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+    $camp = array_column($result,'camp');
+    return $camp;
+  }
+}
+
+//campリストの取得
+function camp_list(){
+  $pdo = db_conn();
+
+  $stmt = $pdo->prepare("SELECT * FROM camp_table WHERE life=0");
+  $status = $stmt->execute();
+
+  if($status==false) {
+    $error = $stmt->errorInfo();
+    exit("SQLエラー:".$error[2]);
+  }else{
+    $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+    return array_column($result,'camp');
+  }
+}
+
+
+//couseリストの取得
+function course_list(){
+  $pdo = db_conn();
+  $stmt = $pdo->prepare("SELECT * FROM course_table WHERE life=0");
+  $status = $stmt->execute();
+
+  if($status==false) {
+    $error = $stmt->errorInfo();
+    exit("SQLエラー:".$error[2]);
+  }else{
+    $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+    return array_column($result,'course');
+  }
+}
+
+
+//key(kw)データ（事務局コード)
+function kw_c(){
+  $pdo = db_conn();
+  $stmt = $pdo->prepare("SELECT * FROM key_table WHERE id = '1'");
+  $status = $stmt->execute();
+
+  if($status==false) {
+    $error = $stmt->errorInfo();
+    exit("SQLエラー:".$error[2]);
+  }else{
+    $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+    return array_column($result,'kw');
+  }
+}
+
+
+
+
+
+?>
---
+++ src/php/valid.php
@@ -0,0 +1,77 @@
+
+<?php
+
+// バリテーション（文字数で制御）
+function valc($postname,$min,$max){
+  $val = filter_input(INPUT_POST, $postname);
+  $n = strlen($val);
+
+  if ($n == 0){
+    $result = '<div class="alert alert-danger" role="alert">未入力です。';
+    return $result;
+
+  }elseif ($n > $max){
+    $n = $n - $max;
+    $result = $n."文字多いです。<br>";
+    return $result;
+
+  }elseif ($n < $min) {
+    $n = $min - $n;
+    $result = $n."文字少ない。<br>";
+    return $result;
+  }
+}
+
+
+// メアド確認用
+function mailvc($postname){
+  $val = filter_input(INPUT_POST, $postname);
+
+  if (!preg_match('/^([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}$/iD', $val)){
+    return '<div class="alert alert-danger" role="alert">【メールアドレスが正しくありません】';
+  }
+}
+
+// パスワード用(半角英数 4以上8以下)
+function passvc($postname){
+  $val = filter_input(INPUT_POST, $postname);
+
+  if (!preg_match("/^[a-zA-Z0-9]{4,8}+$/", $val)){
+    return '<div class="alert alert-danger" role="alert">【パスワードは4文字以上8文字以下の英数字でお願いします】';
+  }
+}
+
+// 入力確認用
+function inputconf($postname,$postname2){
+  $val1 = filter_input(INPUT_POST, $postname);
+  $val2 = filter_input(INPUT_POST, $postname2);
+
+  if ($val1 !== $val2){
+    return '<div class="alert alert-danger" role="alert">【入力に差異がありますか】';
+  }
+}
+
+
+// メアド二重チェック
+function mail_double_check($postname) {
+
+    $mail = filter_input(INPUT_POST, $postname);
+
+  $pdo = db_conn();
+  $stmt = $pdo->prepare("SELECT * FROM user_table WHERE  mail=:mail limit 1");
+  $stmt->bindValue(':mail', $mail, PDO::PARAM_STR);      //Integer（数値の場合 PDO::PARAM_INT)
+  $status = $stmt->execute();
+
+  if($status==false) {
+    $error = $stmt->errorInfo();
+    exit("SQLエラー:".$error[2]);
+  }else{
+    $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+
+    if(empty($result)){
+    }else{
+      return "【そのメールアドレスは登録ずみです】";
+    }
+
+  }
+}
