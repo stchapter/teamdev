55a08a7cfa1a0ecd13f40190d7b025f2a74e9cdf
jun <junf2012@gmail.com>
2021-06-24 12:17:06 +0900

    ログインlifeフラグ機能

--- src/php/login_act.php
+++ src/php/login_act.php
@@ -10,10 +10,7 @@ session_start();
 //POST値
 $mail = $_POST["mail"];
 $pw = $_POST["pw"];
-
-echo $pw;
-
-
+// echo $pw;
 
 // DB接続
 $pdo = db_conn();
@@ -34,35 +31,48 @@ $val = $stmt->fetch();         //1レコードだけ取得
 v($val);
 
 
-if(password_verify($pw, $val['pw'])){
+$life = $val['life'];
 
-    // //該当レコードがあれば、DBから取得した$valの値をSESSIONに値を代入
-    if( $val["id"] != "" ){
-      //Login成功時
-      $_SESSION["chk_ssid"]  = session_id();
-      $_SESSION["name"] = $val['name'];
-      $_SESSION["camp"] = $val['camp'];
-      $_SESSION["course"] = $val['course'];
-      $_SESSION["cls"] = $val['cls'];
-      $_SESSION["student"] = $val['student'];
-      $_SESSION["mail"] = $val['mail'];
-      $_SESSION["kanri"] = $val['kanri'];
-      $_SESSION["life"] = $val['life'];
-      $_SESSION["udate"] = $val['udate'];
-
-      echo "成功";
-      v($_SESSION["name"]);
-
-      // redirect("main.php");
-
-    }else{
-      //Login失敗時
-      redirect("../../public/login.php");
-    }
+// 退会済み
+if ($life==1) {
+  // redirect('"../../public/login.php"');
+  echo "退会済";
 
 }else{
-      $_SESSION["emess"]='<div class="alert alert-danger" role="alert">IDとパスワードが異なります<br></div>';
-      redirect("../../public/login.php");
+
+  if(password_verify($pw, $val['pw'])){
+
+      // //該当レコードがあれば、DBから取得した$valの値をSESSIONに値を代入
+      if( $val["id"] != "" ){
+        //Login成功時
+        $_SESSION["chk_ssid"]  = session_id();
+        $_SESSION["name"] = $val['name'];
+        $_SESSION["camp"] = $val['camp'];
+        $_SESSION["course"] = $val['course'];
+        $_SESSION["cls"] = $val['cls'];
+        $_SESSION["student"] = $val['student'];
+        $_SESSION["mail"] = $val['mail'];
+        $_SESSION["kanri"] = $val['kanri'];
+        $_SESSION["life"] = $val['life'];
+        $_SESSION["ipass"] = $val['ipass'];
+        $_SESSION["udate"] = $val['udate'];
+
+        echo "成功";
+        v($_SESSION["name"]);
+
+        // redirect("main.php");
+
+      }else{
+        //Login失敗時
+        redirect("../../public/login.php");
+      }
+
+  }else{
+        $_SESSION["emess"]='<div class="alert alert-danger" role="alert">IDとパスワードが異なります<br></div>';
+        redirect("../../public/login.php");
+  }
+
 }
+
 
 ?>
