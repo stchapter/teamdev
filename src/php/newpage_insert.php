844633e2f3aed13e2eb813f0117d7ba9305e4427
atuki <stchapter31@gmail.com>
2021-06-25 00:55:34 +0900

    ５２ーnewpageのバリ修正

--- src/php/newpage_insert.php
+++ src/php/newpage_insert.php
@@ -2,8 +2,8 @@
 //session check//
 //session_start();
 //sschk();
-include("../src/php/funcs.php");
-include("../src/php/db.php");
+include("funcs.php");
+include("db.php");
 
 
 //1. POSTデータ取得
@@ -14,21 +14,8 @@ $url      = $_POST["url"];
 $cost     = $_POST["cost"];
 $post     = $_POST["post"];
 $star     = $_POST["star"];
-$fpass    = fileUpload("upfile","../upload/");
+$fpass    = fileUpload("upfile","../../upload/");
 $fname    = $_FILES["upfile"]["name"];
-$err_msgs = array();
-
-
-if($lang == "選択してください"){
-  array_push($err_msgs,'言語を選択してください<br><br>');
-}
-if(strlen($title) == 0){
-  array_push($err_msgs,'タイトルを入力してください<br><br>');
-}
-if(strlen($cont) == 0){
-  array_push($err_msgs,'おすすめ内容を入力してください<br><br>');
-}
-if(count($err_msgs) === 0) {
 
 
 /* ------------------------------------------------
@@ -53,29 +40,11 @@ if(count($err_msgs) === 0) {
     $stmt->bindValue(':fname', $fname, PDO::PARAM_STR);      //Integer（数値の場合 PDO::PARAM_INT)
     $stmt->bindValue(':star', $star, PDO::PARAM_STR);      //Integer（数値の場合 PDO::PARAM_INT)
     $status = $stmt->execute(); //実行
-    echo '<p>登録が完了しました</p>';
-    echo '<a href="newpage.php">登録画面に戻る</a>';
+
     /* ------------------------------------------------
     //////DBにデータを保存 end
     ------------------------------------------------ */
 
-} else{
-  foreach ($err_msgs as $msg){
-    echo $msg;
-    echo '<br>';
-  }
-  echo '<a href="newpage.php">登録画面に戻る</a><br><br>';
-}
+    redirect('../../public/newpage.php');
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>新規投稿</title>
-    </head>
-<body">
-
-</body>
-</html>
