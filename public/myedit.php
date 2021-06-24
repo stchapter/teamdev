e172af2fd68c01a40afc84ebaf12f046793024ec
sakura miyamoto <sakura.m.208@gmail.com>
2021-06-24 18:42:17 +0900

    myedit.phpのフロントビュー作成

--- public/myedit.php
+++ public/myedit.php
@@ -7,7 +7,7 @@ include("../src/php/db.php");
 
 
 // $id = $_GET["id"];
-$id = 20;          //test用にidを20に固定
+$id = 1;          //test用にidを20に固定
 $pdo = db_conn();
 
 //２．データ登録SQL作成
@@ -47,45 +47,117 @@ while( $res = $stmt->fetch(PDO::FETCH_ASSOC)){
 ?>
 
 
-
 <!DOCTYPE html>
 <html lang="ja">
-    <head>
-        <meta charset="UTF-8">
-        <title>投稿の修正</title>
-    </head>
-    <body>
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/fomantic-ui/2.8.7/semantic.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fomantic-ui/2.8.7/semantic.min.css" media="all">
+  <link rel="icon" href="../img/favicon.ico">
+  <link rel="stylesheet" href="../src/css/main.css">
+  <title>GEEKBOOK</title>
+</head>
+<body>
+
+  <header>
+  <div class="header_container">
+
+    <div class="header_logo_container">
+      <div class="header_logo">
+        <img src="../img/topImg.png">
+      </div>
+      <p class="login_name">ログインしている人の名前　さん</p>
+    </div>
+
+
+    <div class="header_button">
+      <div class="header_button_container">
+        <div class="blue ui buttons">
+          <a class="ui button">TOPへ</a>
+          <a class="ui button">登録修正</a>
+          <a class="ui button">新規投稿</a>
+          <a class="ui button">自分の投稿</a>
+          <a class="ui button">Bookmark</a>
+  	  <div class="header_button_R">
+            <a class="ui button">Logout</a>
+	  </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  </header>
+
+    <div class="editor_container">
+        <h2 class="editor_title">投稿の修正</h2>
         <form method="POST" action="../src/php/myedit_update.php" enctype="multipart/form-data">
-            <p>言語:
+
+            <div class="contents">
+            <p>タイトル
+                <div class="ui input text_input">
+                <input type="text" name="title" placeholder="64文字以内で入力してください" value=<?=$row["title"]?>>
+                </div>
+            </p>
+            </div>
+
+
+            <div class="contents">
+            <p>言語
+            <div class="select">
                 <select name="lang">
                     <option value ="<?=$row["lang"]?>" selected><?=$row["lang"]?>(変更なし)</option>
                     <?=$view?>
                 </select>
+            </div>
             </p>
-            <p>タイトル(機能):
-                <input type="text" name="title" placeholder="64文字以内で入力してください" value=<?=$row["title"]?>>
-            </p>
-            <p>おすすめ内容:
+            </div>
+
+            <div class="contents">
+            <p>おすすめ内容
                 <textarea name="cont" rows="4" clos="50"><?=$row["cont"]?></textarea>
             </p>
+            </div>
+
+            <div class="contents_fix">
+            <div class="contents">
             <p>URL
+                <div class="ui input text_input">
                 <input type="url" name="url" value=<?=$row["url"]?>>
+                </div>
             </p>
+                </div>
+
+            <div class="contents">
             <p>有料・無料
+            <div class="select">
                 <select name="cost">
                     <option value ="<?=$row["cost"]?>" selected><?=$row["cost"]?>(変更なし)</option>
                     <option value ="有料">有料</option>
                     <option value ="無料">無料</option>
                 </select>
+            </div>
             </p>
+            </div>
+            </div>
+
+            <div class="contents_fix">
+            <div class="contents">
             <p>投稿
+            <div class="select">
                 <select name="post">
                     <option value ="<?=$row["post"]?>" selected><?=$row["post"]?>(変更なし)</option>
                     <option value ="投稿">投稿</option>
                     <option value ="下書き保存">下書き保存</option>
                 </select>
+            </div>
             </p>
+            </div>
+
+            <div class="contents">
             <p>おすすめ度
+            <div class="select">
                 <select name="star">
                     <option value ="<?=$row["star"]?>" selected><?=$row["star"]?>(変更なし)</option>
                     <option value ="★★★★★">★★★★★</option>
@@ -94,14 +166,22 @@ while( $res = $stmt->fetch(PDO::FETCH_ASSOC)){
                     <option value ="★★☆☆☆">★★☆☆☆</option>
                     <option value ="★☆☆☆☆">★☆☆☆☆</option>
                 </select>
+            </div>
             </p>
+            </div>
+            </div>
+
+            <div class="contents">
             <p>添付ファイル
                 <input type="file" accept="image/*, .pdf, .jpg, .jpeg, .png" name="upfile" value=<?=$row["fname"]?>>
             </p>
-            <p>現在の添付ファイルを変更する場合は、改めて選択願います。</p>
-            <p>未選択の場合は、現在の添付ファイルがそのまま維持されます。</p>
-            <input class="cost_submit" type="submit" value="投稿">
+            <p class="input_text">現在の添付ファイルを変更する場合は、改めて選択願います。</p>
+            <p class="input_text">未選択の場合は、現在の添付ファイルがそのまま維持されます。</p>
+            </div>
+
+            <input class="cost_submit ui primary button" type="submit" value="更新" style="margin-top: 16px;">
         </form>
+    </div>
 
 
         <!-- <form enctype="multipart/form-data" method="POST" action="update.php">
@@ -120,9 +200,10 @@ while( $res = $stmt->fetch(PDO::FETCH_ASSOC)){
         </form> -->
 
         <!-- Main[End] -->
-
+  <footer>
+    <div class="footer">
+      <p>copyright ©️ GEEKBOOK <br> For G's Academy</p>
+    </div>
+  </footer>
     </body>
 </html>
-
-
-
