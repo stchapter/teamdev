ee31c0c15a6edeb5ebd3fd8b70bbd54d768cbe5a
sakura miyamoto <sakura.m.208@gmail.com>
2021-06-26 17:33:05 +0900

    update_usearchフロントビュー作成

--- public/update_usearch.php
+++ public/update_usearch.php
@@ -40,39 +40,72 @@ if ($_POST) {
 
 }
 
+include("./instance/header.php");
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<div class="ke_container">
+
+    <div class="ke_card">
+
     <form action="" method="POST">
-        名前：<?= $user['name'] ?><br>
-        校舎：<?= $user['camp'] ?><br>
-        コース：<?= $user['course'] ?><br>
-        クラス：<?= $user['cls'] ?><br>
-        在籍番号：<?= $user['student'] ?><br>
-        <label>
-            管理：
-            <select name="kanri" id="kanri">
-                <option value="0" <?= $user['kanri'] === '0' ? 'selected' : '' ?>>一般</option>
-                <option value="1" <?= $user['kanri'] === '1' ? 'selected' : '' ?>>管理者</option>
-            </select>
-        </label><br>
-        <label>
-            在籍：
-            <select name="life" id="life">
-                <option value="0" <?= $user['life'] === '0' ? 'selected' : '' ?>>在籍</option>
-                <option value="1" <?= $user['life'] === '1' ? 'selected' : '' ?>>離籍</option>
-            </select>
-        </label><br>
-        <input type="submit" value="更新">
+
+        <div class="ke_contents_fix">
+
+            <p style="font-size: 40px;
+                      font-weight: bold;"><?= $user['name'] ?></p>
+
+            <p style="font-size: 40px;
+                      font-weight: bold;
+                      margin-left: 100px;"><?= $user['camp'] ?> 校</p>
+        </div>
+        <div class="ke_contents_fix1">
+            <p style="font-size: 50px;
+                      font-weight: bold;"><?= $user['course'] ?></p>
+            <p style="font-size: 30px;
+                      font-weight: bold;
+                      margin-left: 60px;"><?= $user['cls'] ?> th</p>
+            <p style="font-size: 30px;
+                      font-weight: bold;
+                      margin-left: 60px;">No. <?= $user['student'] ?></p>
+        </div>
+
+         <div class="ke_contents_fix">
+
+            <div class="ke_div">
+                <p class="ke_p">管理権限</p>
+                <div class="ke_select">
+                    <select style="background-color: #dee7f0;" name="kanri" id="kanri">
+                        <option value="0" <?= $user['kanri'] === '0' ? 'selected' : '' ?>>一般</option>
+                        <option value="1" <?= $user['kanri'] === '1' ? 'selected' : '' ?>>管理者</option>
+                    </select>
+                </div>
+            </div>
+            <div class="ke_div">
+                <p class="ke_p"> 在籍</p>
+                <div class="ke_select">
+                <select style="background-color: #dee7f0;" name="life" id="life">
+                    <option value="0" <?= $user['life'] === '0' ? 'selected' : '' ?>>在籍</option>
+                    <option value="1" <?= $user['life'] === '1' ? 'selected' : '' ?>>離籍</option>
+                </select>
+                </div>
+            </div>
+
+        </div>
+
+        <button
+        class="big ui blue button"
+        type="submit"
+         value="更新"
+        style="margin-top: 60px;">更新</button>
     </form>
-</body>
-</html>
+        <!-- <div class="ke_logo">
+            <img src="../img/stanp.png">
+        </div> -->
+    </div>
+
+</div>
+
+<?php
+include("./instance/footer.php");
+?>
